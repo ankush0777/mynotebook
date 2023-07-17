0e2c0cbc0ac36da6964d4d6d087c90185fda51e1
ankush0777 <130659966+ankush0777@users.noreply.github.com>
2023-07-17 17:38:19 +0530

    first-commit

---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+// import { useContext } from 'react'
+// import NoteContext from '../context/notes/NoteContext'
+const About = () => {
+  
+  return (
+    <div>Hi we never share your notes with anyone else</div>
+  )
+}
+
+export default About
